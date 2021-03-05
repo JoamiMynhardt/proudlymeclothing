ac1a16348a1b1daf9c273c195f3858259e91e00b
JoamiMynhardt <joamimynhardt@gmail.com>
2021-03-05 17:40:29 +0200

    This is the first offical push

---
+++ fileserver/vfm-admin/users/users.php
@@ -0,0 +1,17 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$9y4Ye8S3$c8H0eqTI73bQvnegkXDJ6/',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'Joami',
+    'pass' => '$1$nmeIJSv1$Q0c2jP6osdBmGBuuce5o0/',
+    'role' => 'superadmin',
+    'email' => 'joamimynhardt@gmail.com',
+  ),
+);
